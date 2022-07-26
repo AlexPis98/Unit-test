0134e6d3e9f3400693be19d84d907b7c7ed30085
Boris Rumyantsev <bd.rumyantsev@gmail.com>
2022-07-26 15:08:22 +0300

    fix bignumber error

--- test/test.js
+++ test/test.js
@@ -1,15 +1,17 @@
 const { DAI, token_decimals, tokenToNETH } = require('../index');
-var BigNumber = require('bignumber.js');
 
 var chai = require('chai');
-chai.use(require('chai-bignumber')(BigNumber));
+var assert = chai.assert;
 
 describe('tokenToNeth', () => {
   it('should return neth for token', async () => {
+      var wrap_to_int = (a) => BigInt(parseInt(String(exp).replace('.','')));
       let count = 1000;
       let exp = 0.001165865684468688;
+      exp = wrap_to_int(exp)
       let res = await tokenToNETH(count, DAI, token_decimals);
-      res.should.be.bignumber.approximately(exp, 0.001);
-      // res.should.be.equal(exp);
+      console.log(exp)
+      res = wrap_to_int(res)
+      assert.equal(exp,res)
     });
 });
